cbc53c1d888d21d048cb7c92256a931a02451a97
Mustafaiq1998x <154765829+Mustafaiq1998x@users.noreply.github.com>
2026-03-06 09:53:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/",
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -19,6 +21,7 @@ export default defineConfig({
         ]
       : []),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -26,11 +29,18 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
+
   root: path.resolve(import.meta.dirname, "client"),
+
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
+
+  define: {
+    CESIUM_BASE_URL: JSON.stringify("/cesium"),
+  },
+
   server: {
     fs: {
       strict: true,
